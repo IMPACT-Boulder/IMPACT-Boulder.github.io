e517e433bf832aa7991bd7eaa17aa6aa3f793b37
Aaron <kesslerawkessler@gmail.com>
2023-05-10 16:28:45 -0600

    responsive design updates

--- layout/scripts/effects.js
+++ layout/scripts/effects.js
@@ -1,4 +1,6 @@
  ////scroll effect//////
+
+ //homepage toggle variable
  let checkbox=document.querySelector('#menu__toggle');
  //distance required to scroll to cause effect
  //secondary pages scroll threshold
@@ -6,42 +8,39 @@ const SCROLL_THRESHOLD = 30;
  //home page scroll threshold
 const SCROLL_THRESHOLD_HOME = 70;
 
-
-
+//scroll variable
  let lastScrollY = window.scrollY;
- 
+
+ //secondary pages event listener
  window.addEventListener('scroll', () => {
    const currentScrollY = window.scrollY;
    const delta = currentScrollY - lastScrollY;
  
    if (delta > 0 && delta >= SCROLL_THRESHOLD) {
      // The user has scrolled down by at least SCROLL_THRESHOLD pixels
+     //add nav_up class
      document.querySelector(".the-header").classList.add("nav_up")
      document.querySelector(".menu__box").classList.add("nav_up")
 
    } else if (delta < 0 && -delta >= SCROLL_THRESHOLD) {
      // The user has scrolled up by at least SCROLL_THRESHOLD pixels
+     //remove nav_up class
      document.querySelector(".the-header").classList.remove("nav_up")
      document.querySelector(".menu__box").classList.remove("nav_up")
    }
  
    lastScrollY = currentScrollY;
  });
-
 /////////////////////////////////////////////////////////////
+//homepage event listener
 window.addEventListener('scroll', () => {
   const currentScrollY = window.scrollY;
   const delta = currentScrollY - lastScrollY;
 
   if (delta > 0 && delta >= SCROLL_THRESHOLD_HOME) {
     // The user has scrolled down by at least SCROLL_THRESHOLD pixels
-    
-    document.querySelector(".menu__box__home").classList.add("nav_up")
+    // remove menu toggle
     checkbox.checked=false;
-  } else if (delta < 0 && -delta >= SCROLL_THRESHOLD) {
-    // The user has scrolled up by at least SCROLL_THRESHOLD pixels
-
-    document.querySelector(".menu__box__home").classList.remove("nav_up")
   }
 
   lastScrollY = currentScrollY;
