b0d749924ef901e04d94a6cc373b923f4332ec1e
Aaron <kesslerawkessler@gmail.com>
2023-05-23 15:17:33 -0600

    website fixes

--- layout/scripts/homeEffects.js
+++ layout/scripts/homeEffects.js
@@ -1,24 +1,25 @@
-//home scroll nav
-//homepage toggle variable
-let checkbox=document.querySelector('#menu__toggle');
-//distance required to scroll to cause effect
-//home page scroll threshold
-const SCROLL_THRESHOLD_HOME = 70;
-
-//scroll variable
-let lastScrollY = window.scrollY;
-
-/////////////////////////////////////////////////////////////
-//homepage event listener
-window.addEventListener('scroll', () => {
- const currentScrollY = window.scrollY;
- const delta = currentScrollY - lastScrollY;
-
- if (delta > 0 && delta >= SCROLL_THRESHOLD_HOME) {
-   // The user has scrolled down by at least SCROLL_THRESHOLD pixels
-   // remove menu toggle
-   checkbox.checked=false;
- }
-
- lastScrollY = currentScrollY;
-});
+ ////scroll effect//////
+ let checkbox=document.querySelector('#menu__toggle')
+ //distance required to scroll to cause effect
+ //secondary pages scroll threshold
+ const SCROLL_THRESHOLD = 70;
+ 
+ //scroll variable
+  let lastScrollY = window.scrollY;
+ 
+  //secondary pages event listener
+  window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+    const delta = currentScrollY - lastScrollY;
+  
+    if (delta > 0 && delta >= SCROLL_THRESHOLD) {
+      // The user has scrolled down by at least SCROLL_THRESHOLD pixels
+      //add nav_up class
+      
+      checkbox.checked=false;
+ 
+    } 
+  
+    lastScrollY = currentScrollY;
+  });
+ /////////////////////////////////////////////////////////////
