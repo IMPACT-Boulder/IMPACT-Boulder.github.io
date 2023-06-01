c90f302b0fdda7dcc19fb9ba73681270b59ea3f1
Aaron <kesslerawkessler@gmail.com>
2023-06-01 12:28:54 -0600

    fix overflow

--- layout/scripts/homeEffects.js
+++ layout/scripts/homeEffects.js
@@ -1,4 +1,4 @@
- ////scroll effect//////
+ ////scroll effect//////*
  let checkbox=document.querySelector('#menu__toggle')
  //distance required to scroll to cause effect
  //secondary pages scroll threshold
@@ -23,3 +23,27 @@
     lastScrollY = currentScrollY;
   });
  /////////////////////////////////////////////////////////////
+ /*const SCROLL_THRESHOLD = 30;
+ 
+//scroll variable
+ let lastScrollY = window.scrollY;
+
+ //secondary pages event listener
+ window.addEventListener('scroll', () => {
+   const currentScrollY = window.scrollY;
+   const delta = currentScrollY - lastScrollY;
+ 
+   if (delta > 0 && delta >= SCROLL_THRESHOLD) {
+     // The user has scrolled down by at least SCROLL_THRESHOLD pixels
+     //add nav_up class
+     document.querySelector(".menu__box").classList.add("nav_up")
+
+   } else if (delta < 0 && -delta >= SCROLL_THRESHOLD) {
+     // The user has scrolled up by at least SCROLL_THRESHOLD pixels
+     //remove nav_up class
+     
+     document.querySelector(".menu__box").classList.remove("nav_up")
+   }
+ 
+   lastScrollY = currentScrollY;
+ });*/
