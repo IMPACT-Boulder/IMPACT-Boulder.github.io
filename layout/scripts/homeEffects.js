6ecdb521dff4c0da8802bd413c52b1fefc1d2191
Aaron Kessler <kesslerawkessler@gmail.com>
2024-01-08 15:21:44 -0700

    automatically sort people

--- layout/scripts/homeEffects.js
+++ layout/scripts/homeEffects.js
@@ -23,27 +23,3 @@
     lastScrollY = currentScrollY;
   });
  /////////////////////////////////////////////////////////////
- /*const SCROLL_THRESHOLD = 30;
- 
-//scroll variable
- let lastScrollY = window.scrollY;
-
- //secondary pages event listener
- window.addEventListener('scroll', () => {
-   const currentScrollY = window.scrollY;
-   const delta = currentScrollY - lastScrollY;
- 
-   if (delta > 0 && delta >= SCROLL_THRESHOLD) {
-     // The user has scrolled down by at least SCROLL_THRESHOLD pixels
-     //add nav_up class
-     document.querySelector(".menu__box").classList.add("nav_up")
-
-   } else if (delta < 0 && -delta >= SCROLL_THRESHOLD) {
-     // The user has scrolled up by at least SCROLL_THRESHOLD pixels
-     //remove nav_up class
-     
-     document.querySelector(".menu__box").classList.remove("nav_up")
-   }
- 
-   lastScrollY = currentScrollY;
- });*/
