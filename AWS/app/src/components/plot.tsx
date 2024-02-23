97d907917f4d481f16b72dc85f05c4c471edad57
Aaron Kessler <kesslerawkessler@gmail.com>
2024-02-22 17:26:49 -0700

    experiment names hover info

--- AWS/app/src/components/plot.tsx
+++ AWS/app/src/components/plot.tsx
@@ -1,3 +1,4 @@
+// plot.tsx
 import React, { useEffect, useState } from 'react';
 import Plotly from 'plotly.js-dist';
 import DropDown from './axisDropdown.tsx';
@@ -20,7 +21,7 @@ const axisLabelMapping: Record<string, string> = {
   'Tag': 'Tag',
 };
 
-const DustPlot: React.FC<DustPlotProps> = ({ numberOfDataValues, data, }) => {
+const DustPlot: React.FC<DustPlotProps> = ({ numberOfDataValues, data }) => {
   const [xAxis, setXAxis] = useState<string>('Mass');
   const [yAxis, setYAxis] = useState<string>('Velocity');
 
@@ -34,61 +35,116 @@ const DustPlot: React.FC<DustPlotProps> = ({ numberOfDataValues, data, }) => {
 
   const [keyVisibility, setKeyVisibility] = useState<'none' | 'block'>('none');
 
-useEffect(() => {
-  if (data.length > 0) {
-    setKeyVisibility('block');
-  } else {
-    setKeyVisibility('none');
-  }
+  useEffect(() => {
+    if (data.length > 0) {
+      setKeyVisibility('block');
+    } else {
+      setKeyVisibility('none');
+    }
 
-  const chartData = data;
+    console.log('Data:', data);
 
-  const limitedDataItems: DataItem[] = chartData.slice(0, numberOfDataValues || 1);
+    const chartData = data;
 
-  console.log('Limited Data Items:', limitedDataItems); // Add this logging statement
+    const limitedVelocities = chartData.map((item) => item['Velocity (km/s)']).slice(0, numberOfDataValues || 1);
+    const limitedMasses = chartData.map((item) => item['Mass (kg)']).slice(0, numberOfDataValues || 1);
+    const limitedCharges = chartData.map((item) => item['Charge (C)']).slice(0, numberOfDataValues || 1);
+    const limitedTraceNumbers = chartData.map((item) => item['Trace Number']).slice(0, numberOfDataValues || 1);
+    const limitedEstimateQualities = chartData.map((item) => item['Estimate Quality']).slice(0, numberOfDataValues || 1);
+    const limitedTimes = chartData.map((item) => item['Time']).slice(0, numberOfDataValues || 1);
+    const limitedRadii = chartData.map((item) => item['Radius (m)']).slice(0, numberOfDataValues || 1);
+    const limitedDataItems: DataItem[] = chartData.map((item) => ({
+      'Mass (kg)': item['Mass'],
+      'Velocity (km/s)': item['Velocity'],
+      'Charge (C)': item['Charge'],
+      'Trace Number': item['Trace Number'],
+      'Radius (m)': item['Radius'],
+      'Estimate Quality': item['Estimate Quality'],
+      'Time': item['Time'],
+      'Dust Name': item['Dust Name'],
+      'Tag': item['Tag'],
+    } as DataItem));
+    
 
-  const plotData = {
-    x: limitedDataItems.map(item => item[xAxis]), // Use dynamic property access to get x values
-    y: limitedDataItems.map(item => item[yAxis]), // Use dynamic property access to get y values
-    mode: 'markers',
-    type: 'scatter',
-    marker: {
-      size: limitedDataItems.map(item => item.Radius * 100000000), // Scale radius directly here
-    },
-    hoverinfo: 'text',
-    text: limitedDataItems.map(item =>
-      `Velocity[ km/s ]: ${item.Velocity}<br>Mass [ kg ]: ${item.Mass}<br>Charge[ C ]: ${item.Charge}<br>Original Radius[ m ]: ${item.Radius}<br>Trace Number: ${item.TraceNumber}<br>Estimate Quality: ${item.EstimateQuality}<br>Velocity[ km/s ]: ${item.Velocity}<br>Time [ MST ]: ${item.Time}<br>Dust Type: ${item.DustName}<br>Tag: ${item.Tag}`,
-    ),
+    const scalingFactor = 100000000;
+    const scaledRadii = limitedRadii.map((radius) => radius * scalingFactor);
+
+    const plotData = [
+      {
+        x: getXAxisData(xAxis, limitedVelocities, limitedCharges, limitedTraceNumbers, limitedRadii, limitedEstimateQualities, limitedMasses, limitedTimes),
+        y: getYAxisData(yAxis, limitedVelocities, limitedCharges, limitedTraceNumbers, limitedRadii, limitedEstimateQualities, limitedMasses, limitedTimes),
+        mode: 'markers',
+        type: 'scatter',
+        marker: {
+          size: scaledRadii,
+        },
+        hoverinfo: 'text',
+        text: scaledRadii.map((radius, index) =>
+          `Velocity[ km/s ]: ${limitedVelocities[index]}<br>Mass [ kg ]: ${limitedMasses[index]}<br>Charge[ C ]: ${limitedCharges[index]}<br>Original Radius[ m ]: ${limitedRadii[index]}<br>Trace Number: ${limitedTraceNumbers[index]}<br>Estimate Quality: ${limitedEstimateQualities[index]}<br>Velocity[ km/s ]: ${limitedVelocities[index]}<br>Time [ MST ]: ${limitedTimes[index]}<br>Dust Type: ${limitedDataItems[index]['Dust Name']}<br>Tag: ${limitedDataItems[index]['Tag']}`,
+        ),
+      },
+    ];
+
+    const layout = {
+      title: 'Dust Plot',
+      xaxis: {
+        zeroline: false,
+        title: getXAxisTitle(xAxis),
+        automargin: true,
+        titlefont: {
+          color: 'black',
+        },
+      },
+      yaxis: {
+        zeroline: false,
+        title: getYAxisTitle(yAxis),
+        automargin: true,
+        titlefont: {
+          color: 'black',
+        },
+      },
+    };
+
+    Plotly.newPlot('dust_plot', plotData, layout);
+  }, [xAxis, yAxis, data, numberOfDataValues]);
+
+  const getXAxisData = (axis: string, velocities: number[], charges: number[], traceNumbers: number[], radii: number[], estimateQualities: number[], masses: number[], times: number[]) => {
+    switch (axis) {
+      case 'Velocity':
+        return velocities;
+      case 'Charge':
+        return charges;
+      case 'Trace Number':
+        return traceNumbers;
+      case 'Radius':
+        return radii;
+      case 'Estimate Quality':
+        return estimateQualities;
+      case 'Time':
+        return times;
+      default:
+        return masses;
+    }
   };
 
-  console.log('Plot Data:', plotData); // Add this logging statement
-
-  const layout = {
-    title: 'Dust Plot',
-    xaxis: {
-      zeroline: false,
-      title: getXAxisTitle(xAxis),
-      automargin: true,
-      titlefont: {
-        color: 'black',
-      },
-    },
-    yaxis: {
-      zeroline: false,
-      title: getYAxisTitle(yAxis),
-      automargin: true,
-      titlefont: {
-        color: 'black',
-      },
-    },
+  const getYAxisData = (axis: string, velocities: number[], charges: number[], traceNumbers: number[], radii: number[], estimateQualities: number[], masses: number[], times: number[]) => {
+    switch (axis) {
+      case 'Velocity':
+        return velocities;
+      case 'Charge':
+        return charges;
+      case 'Trace Number':
+        return traceNumbers;
+      case 'Radius':
+        return radii;
+      case 'Estimate Quality':
+        return estimateQualities;
+      case 'Time':
+        return times;
+      default:
+        return masses;
+    }
   };
-
-  console.log('Layout', layout); // Add this logging statement
-
-  Plotly.newPlot('dust_plot', [plotData], layout); // Wrap plotData in an array
-
-}, [xAxis, yAxis, data, numberOfDataValues]);
-
 
   const getXAxisTitle = (axis: string) => {
     return axis === '' ? '' : axisLabelMapping[axis] || axis;
