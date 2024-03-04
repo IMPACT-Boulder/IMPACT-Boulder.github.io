606848cfb53471f6f50ed54963d3dcc9c4c9e6ea
Aaron Kessler <kesslerawkessler@gmail.com>
2024-03-04 10:40:50 -0700

    build repair

--- AWS/app/src/components/DataInputControl.tsx
+++ AWS/app/src/components/DataInputControl.tsx
@@ -1,4 +1,3 @@
-//DataInputControl.tsx
 import React, { useState } from 'react';
 import TextBox from './inputLimit.tsx';
 import VelocityLow from './VelocityRange/VelocityLow.tsx';
@@ -18,7 +17,7 @@ import GroupName from './GroupName.tsx';
 import TagDropdown from './TagDropDown.tsx';
 import { convertToCSV } from './CSVUtils.ts';
 import Button from '@mui/material/Button';
-import '../styles/Controls.css'
+import '../styles/Controls.css';
 
 interface DataInputControlProps {
   onDataUpdate: (data: any[]) => void;
@@ -63,43 +62,11 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
     tagNames: [],
   });
 
-
-  // useEffect(() => {
-  //   const fetchDustTypes = async () => {
-  //     try {
-  //       const response = await fetch('http://127.0.0.1:5000/api/dust_types');
-  //       const data: { dust_name: string }[] = await response.json();
-  //       setDustTypeOptions(data.map((item) => item.dust_name));
-  //     } catch (error) {
-  //       console.error('Error fetching dust types:', error);
-  //     }
-  //   };
-
-  //   fetchDustTypes();
-  // }, []);
-
-  // useEffect(() => {
-  //   const fetchGroupNames = async () => {
-  //     try {
-  //       const response = await fetch('http://127.0.0.1:5000/api/group_names');
-  //       const data: { group_name: string }[] = await response.json();
-  //       const sortedGroupNames = data.map((item) => item.group_name).sort();
-  //       setGroupNames(sortedGroupNames);
-
-  //     } catch (error) {
-  //       console.error('Error fetching group names:', error);
-  //     }
-  //   };
-  
-  //   fetchGroupNames();
-  // }, []);
-
-  const[loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [fetchTimestamp, setFetchTimestamp] = useState<number | null>(null);
-
   const [selectedTag, setSelectedTag] = useState<string>('');
   const [actualArray, setActualArray] = useState<any[]>([]);
-  
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const handleTagNamesChange = (value: string) => {
     console.log('Selected Tag Names:', value);
@@ -122,7 +89,6 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
     console.log('Selected Group Names:', values);
     setFormState((prevFormState) => ({ ...prevFormState, groupNames: values }));
   };
-  
 
   const handleVelocityHighChange = (high: number) => {
     setFormState((prevFormState) => ({ ...prevFormState, velHigh: high }));
@@ -165,7 +131,7 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
   };
 
   const handleTimeHighChange = (high: number | null) => {
-    setFormState((prevFormState) => ({ ...prevFormState, timeHigh: high !== null ? high : prevFormState.timeHigh,}));
+    setFormState((prevFormState) => ({ ...prevFormState, timeHigh: high !== null ? high : prevFormState.timeHigh }));
   };
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
@@ -175,36 +141,40 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
     }
   };
 
-  const [errorMessage, setErrorMessage] = useState<string>('');
-
   const handleFormSubmit = async () => {
     try {
       setLoading(true); // Set loading state to true when form is submitted
-      
+
       const dustTypesParam = formState.dustTypes.join(',');
       const groupNamesParam = formState.groupNames.join(',');
       const tagNamesParam = selectedTag;
-      const apiUrl = `https://10.247.29.46:3000/api/data?limit=${formState.limitValue}&velocityLow=${(formState.velLow)*1000}&velocityHigh=${(formState.velHigh)*1000}&qualityLow=${formState.qualLow}&qualityHigh=${formState.qualHigh}&massLow=${formState.massLow}&massHigh=${formState.massHigh}&chargeLow=${formState.chargeLow}&chargeHigh=${formState.chargeHigh}&radiusLow=${formState.radiusLow}&radiusHigh=${formState.radiusHigh}&timeLow=${formState.timeLow}&timeHigh=${formState.timeHigh}&dustType=${dustTypesParam}&groupName=${groupNamesParam}&tagName=${tagNamesParam}`;
-      
+      const apiUrl = `https://10.247.29.177:3000/api/data?limit=${formState.limitValue}&velocityLow=${
+        formState.velLow * 1000
+      }&velocityHigh=${formState.velHigh * 1000}&qualityLow=${formState.qualLow}&qualityHigh=${formState.qualHigh}&massLow=${
+        formState.massLow
+      }&massHigh=${formState.massHigh}&chargeLow=${formState.chargeLow}&chargeHigh=${formState.chargeHigh}&radiusLow=${
+        formState.radiusLow
+      }&radiusHigh=${formState.radiusHigh}&timeLow=${formState.timeLow}&timeHigh=${formState.timeHigh}&dustType=${dustTypesParam}&groupName=${groupNamesParam}&tagName=${tagNamesParam}`;
+
       const response = await fetch(apiUrl);
       const rawData: string = await response.json();
       const parsedArray: any[] = JSON.parse(rawData);
-  
+
       if (Array.isArray(parsedArray)) {
         onDataUpdate(parsedArray);
         setActualArray(parsedArray);
       } else {
         console.error('Data is not an array:', parsedArray);
+        setErrorMessage('Error: Data is not in the expected format');
       }
       setFetchTimestamp(Date.now());
     } catch (error) {
       console.error('Error fetching data:', error);
+      setErrorMessage('Error: Failed to fetch data');
     } finally {
       setLoading(false); // Set loading state to false when fetch operation completes
     }
   };
-  
-  
 
   const handleDownload = () => {
     // Check if data is available
@@ -212,7 +182,7 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
       try {
         // Convert data to CSV
         const csvData = convertToCSV(actualArray);
-  
+
         // Download the CSV file
         const blob = new Blob([csvData], { type: 'text/csv' });
         const url = window.URL.createObjectURL(blob);
@@ -224,36 +194,38 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
         document.body.removeChild(a);
       } catch (error) {
         console.error('Error converting data to CSV:', error);
+        setErrorMessage('Error: Failed to download data as CSV');
       }
     } else {
       console.error('No data available to download.');
+      setErrorMessage('Error: No data available to download');
     }
   };
 
   return (
-    <div id='box'>
+    <div id="box">
       <form onSubmit={handleFormSubmit}>
-        <div id='controls' onKeyDown={handleKeyDown}>
-          <div id='limit_box'>
+        <div id="controls" onKeyDown={handleKeyDown}>
+          <div id="limit_box">
             <TextBox onChange={handleLimitValueChange} limitValueProp={formState.limitValue} />
           </div>
-          <div id='constraints'>
-            <div id='constraints_top'>
-              <DustType onChange={handleDustTypeChange} selectedTypes={formState.dustTypes}/>
-              <GroupName onChange={handleGroupNamesChange} selectedGroups={formState.groupNames}/>
+          <div id="constraints">
+            <div id="constraints_top">
+              <DustType onChange={handleDustTypeChange} selectedTypes={formState.dustTypes} />
+              <GroupName onChange={handleGroupNamesChange} selectedGroups={formState.groupNames} />
               <TagDropdown onChange={handleTagNamesChange} selectedTag={selectedTag} />
             </div>
-            <div id='constraints_main'>
-              <div  className='controls'>
+            <div id="constraints_main">
+              <div className="controls">
                 <h3>Lower Constraints</h3>
-                <VelocityLow onChange={handleVelocityLowChange} velLowProp={formState.velLow}/>
+                <VelocityLow onChange={handleVelocityLowChange} velLowProp={formState.velLow} />
                 <QualityLow onChange={handleQualityLowChange} qualLowProp={formState.qualLow} />
                 <MassLow onChange={handleMassLowChange} massLowProp={formState.massLow} />
                 <ChargeLow onChange={handleChargeLowChange} chargeLowProp={formState.chargeLow} />
                 <RadiusLow onChange={handleRadiusLowChange} radiusLowProp={formState.radiusLow} />
                 <TimeLow onChange={handleTimeLowChange} timeLowProp={formState.timeLow} />
               </div>
-              <div className='controls'>
+              <div className="controls">
                 <h3>Upper Constraints</h3>
                 <VelocityHigh onChange={handleVelocityHighChange} velHighProp={formState.velHigh} />
                 <QualityHigh onChange={handleQualityHighChange} qualHighProp={formState.qualHigh} />
@@ -264,16 +236,16 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
               </div>
             </div>
           </div>
-          <Button type="button" variant="contained" onClick={handleFormSubmit} id='submit'>
+          <Button type="button" variant="contained" onClick={handleFormSubmit} id="submit">
             Submit
           </Button>
-          <div id='loading'>
+          <div id="loading">
             {loading && <p>Loading...</p>}
             {!loading && fetchTimestamp !== null && <p>Data fetched at: {new Date(fetchTimestamp).toLocaleString()}</p>}
           </div>
         </div>
-        <Button type="button" variant="outlined" onClick={handleDownload} id='download'>
-             Download Data as CSV
+        <Button type="button" variant="outlined" onClick={handleDownload} id="download">
+          Download Data as CSV
         </Button>
       </form>
       {errorMessage && (
@@ -284,6 +256,5 @@ const DataInputControl: React.FC<DataInputControlProps> = ({ onDataUpdate }) => 
     </div>
   );
 };
-
 
 export default DataInputControl;
--- AWS/app/src/components/GroupName.tsx
+++ AWS/app/src/components/GroupName.tsx
@@ -1,5 +1,3 @@
-// // GroupName.tsx
-
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
@@ -17,12 +15,12 @@ interface GroupNameProps {
 const GroupName: React.FC<GroupNameProps> = ({ onChange, selectedGroups }) => {
   const [groupsInputValue, setGroupsInputValue] = useState<string[]>(selectedGroups.map(String));
   const [groupNames, setGroupNames] = useState<string[]>([]);
-  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchGroupNames = async () => {
       try {
-        const response = await fetch('https://10.247.29.46:3000/api/group_names');
+        const response = await fetch('https://10.247.29.177:3000/api/group_names');
         if (!response.ok) {
           throw new Error('Failed to fetch');
         }
@@ -32,7 +30,7 @@ const GroupName: React.FC<GroupNameProps> = ({ onChange, selectedGroups }) => {
         setGroupNames(sortedGroupNames);
       } catch (error) {
         console.error('Error fetching group names:', error);
-        setErrorMessage('Cannot Connect to Server');
+        setError('Failed to fetch group names');
       }
     };
 
@@ -56,112 +54,30 @@ const GroupName: React.FC<GroupNameProps> = ({ onChange, selectedGroups }) => {
       noValidate
       autoComplete="off"
     >
-      {errorMessage && <p>Error: {errorMessage}</p>}
-      {groupNames.length > 0 && (
-        <FormControl sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel id="label-group-name">Experiment Group</InputLabel>
-          <Select
-            labelId="label-group-name"
-            id="select-group-name"
-            value={groupsInputValue[0] || ''}
-            label="Experiment Group"
-            onChange={(e: SelectChangeEvent<string | string[]>) => handleDropDownChange(e.target.value)}
-            onBlur={handleBlur}
-          >
-            {groupNames.map((groupName, index) => (
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="label-group-name">Experiment Group</InputLabel>
+        <Select
+          labelId="label-group-name"
+          id="select-group-name"
+          value={groupsInputValue[0] || ''}
+          label="Experiment Group"
+          onChange={(e: SelectChangeEvent<string | string[]>) => handleDropDownChange(e.target.value)}
+          onBlur={handleBlur}
+        >
+          {error ? (
+            <MenuItem disabled>{error}</MenuItem>
+          ) : (
+            groupNames.map((groupName, index) => (
               <MenuItem key={groupName} value={groupName}>
                 {groupName}
               </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText></FormHelperText>
-        </FormControl>
-      )}
+            ))
+          )}
+        </Select>
+        <FormHelperText></FormHelperText>
+      </FormControl>
     </Box>
   );
 };
 
 export default GroupName;
-
-// import React, { useState, useEffect } from 'react';
-// import Box from '@mui/material/Box';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import Select, { SelectChangeEvent } from '@mui/material/Select';
-// import InputLabel from '@mui/material/InputLabel';
-// import FormHelperText from '@mui/material/FormHelperText';
-// import { sortBy } from 'lodash';
-
-// interface GroupNameProps {
-//   onChange: (groups: string[]) => void;
-//   selectedGroups: string[];
-// }
-
-// const GroupName: React.FC<GroupNameProps> = ({ onChange, selectedGroups }) => {
-//   const [groupsInputValue, setGroupsInputValue] = useState<string[]>(selectedGroups.map(String));
-//   const [groupNames, setGroupNames] = useState<string[]>([]);
-//   const [errorMessage, setErrorMessage] = useState<string>('');
-
-//   useEffect(() => {
-//     const fetchGroupNames = async () => {
-//       try {
-//         const response = await fetch('127.0.0.1:5000/api/group_names');
-//         const data: { group_names: string[] }[] = await response.json();
-
-//         const extractedGroupNames = data.map((item) => item.group_names).flat();
-//         const sortedGroupNames = sortBy(extractedGroupNames);
-
-//         // Prepend 'All' to the sorted group names
-//         const groupNamesWithAll = ['All', ...sortedGroupNames];
-//         setGroupNames(groupNamesWithAll);
-//       } catch (error) {
-//         console.error('Error fetching group names:', error);
-//         setErrorMessage('Cannot Connect to Server');
-//       }
-//     };
-
-//     fetchGroupNames();
-//   }, []);
-
-//   const handleDropDownChange = (value: string | string[]) => {
-//     setGroupsInputValue(Array.isArray(value) ? value : [value]);
-//   };
-
-//   const handleBlur = () => {
-//     onChange(groupsInputValue);
-//   };
-
-//   return (
-//     <Box
-//       component="form"
-//       sx={{
-//         '& > :not(style)': { m: 1, width: '20ch' },
-//       }}
-//       noValidate
-//       autoComplete="off"
-//     >
-//       {groupNames.length > 0 && (
-//         <FormControl sx={{ m: 1, minWidth: 120 }}>
-//           <InputLabel id="label-group-name">Experiment Group</InputLabel>
-//           <Select
-//             labelId="label-group-name"
-//             id="select-group-name"
-//             value={groupsInputValue[0] || ''}
-//             label="Experiment Group"
-//             onChange={(e: SelectChangeEvent<string | string[]>) => handleDropDownChange(e.target.value)}
-//             onBlur={handleBlur}
-//           >
-//             {groupNames.map((groupName, index) => (
-//               <MenuItem key={groupName} value={groupName}>
-//                 {groupName}
-//               </MenuItem>
-//             ))}
-//           </Select>
-//           <FormHelperText></FormHelperText>
-//         </FormControl>
-//       )}
-//     </Box>
-//   );
-// };
-
-// export default GroupName;
